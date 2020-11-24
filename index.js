f8efa1e4ecf60d9721c1c1c97757f898c53675c6
mapleincode <llkj13579@126.com>
2020-11-24 17:07:34 +0800

    feat: push

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
  * @Author: maple
  * @Date: 2020-09-18 18:23:15
  * @LastEditors: maple
- * @LastEditTime: 2020-09-18 18:24:10
+ * @LastEditTime: 2020-11-24 17:07:28
  */
 const DDNS = require('wm-ddns').AsyncDomain;
 
-const { email, password, domain, record, loginId, loginToken } = require('./config.json');
+const { email, password, domain, _record, loginId, loginToken } = require('./config.json');
 
 const d = new DDNS(email, password, domain, {
   loginId: loginId,
@@ -16,24 +16,29 @@ const d = new DDNS(email, password, domain, {
 let r;
 let value;
 
-setInterval(async () => {
-  if (!r) {
+const records = [].concat(_record);
+
+for (const record of records) {
+  setInterval(async () => {
+    if (!r) {
+      try {
+        r = await d.recordByNameAsync(record);
+      } catch (err) {
+        console.log('获取 record 失败');
+        console.error(err);
+      }
+    }
+
     try {
-      r = await d.recordByNameAsync(record);
+      await r.ddnsAsync();
     } catch (err) {
-      console.log('获取 record 失败');
+      console.log('更新 ddns 失败');
       console.error(err);
     }
-  }
-  try {
-    await r.ddnsAsync();
-  } catch (err) {
-    console.log('更新 ddns 失败');
-    console.error(err);
-  }
 
-  if (value !== r.value) {
-    console.log('更新 ddns 成功: ' + r.value);
-    value = r.value;
-  }
-}, 1000 * 60);
+    if (value !== r.value) {
+      console.log('更新 ddns 成功: ' + r.value);
+      value = r.value;
+    }
+  }, 1000 * 60);
+}
